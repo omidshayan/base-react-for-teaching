38f91a4d1a8cc91449ad14fc7d6bc8c0ae93fdf9
unknown <omidshayan.afg@gmail.com>
2023-09-25 17:25:07 +0430

    hook setState for plus & minus

--- src/pages/exmaple/Counter.jsx
+++ src/pages/exmaple/Counter.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './counter.css';
 
 
 function Counter() {
+    const [count, setCount] = useState(0);
+
+    function plush (){
+        setCount (count + 1);
+    }
+    function minese (){
+        setCount (count - 1);
+    }
+
   return (
     <div className='counter'>
         <h1>counter</h1>
         <div className="box">
-            <button>
+            <button onClick={plush}>
                 +
             </button>
-            <h3>4</h3>
-            <button>
+            <h3>
+                {count}
+            </h3>
+            <button onClick={minese}>
                 -
             </button>
         </div>
