85b542048224a513a13e5738d3333b081bcb2329
unknown <omidshayan.afg@gmail.com>
2023-09-26 10:45:07 +0430

    preveState in useState

--- src/pages/exmaple/Counter.jsx
+++ src/pages/exmaple/Counter.jsx
@@ -6,24 +6,29 @@ import './counter.css';
 function Counter() {
     const [count, setCount] = useState(0);
 
-    function plush (){
-        setCount (count + 1);
+    const plus = () =>{
+        setCount((prevState) =>{
+            return prevState + 1;
+        });
     }
-    function minese (){
-        setCount (count - 1);
+    const minus = () =>{
+        setCount(prevState => {
+            return prevState -1;
+        })
     }
+
 
   return (
     <div className='counter'>
         <h1>counter</h1>
         <div className="box">
-            <button onClick={plush}>
+            <button onClick={plus}>
                 +
             </button>
             <h3>
                 {count}
             </h3>
-            <button onClick={minese}>
+            <button onClick={minus}>
                 -
             </button>
         </div>
